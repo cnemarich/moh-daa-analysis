157b16431fd174391ea2aecc3e81ef0505588ed8
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2020-04-13 09:45:21 -0400

    Update to how org unit level data is pulled and cleaned for different countries

--- utils.R
+++ utils.R
@@ -83,10 +83,19 @@ analysis_getSitesTable<-function(ou_uid="cDGPF739ZZr") {
 
 analysis_getIndicatorsTable<-function(ou_uid="cDGPF739ZZr") {
   
+  #Countries with facility level at organization unit level 7 
+  level_sevens <- c('Qh4XMQJhbk8','ds0ADyc9UCU','IH1kchw86uA','JTypsdEUNPw',
+                    'HfVjCurKxh2','lZsCb6y0KDX','XtxUYCsDWrR','cDGPF739ZZr',
+                    'WLG0z5NxQs8','mdXu6iCbn2G','FETQ6OmnsKB')
+  
+  ou_lvl <- ifelse(ou_uid %in% level_sevens,7,6)
+    
   base_url<-config$baseurl
   
+  lvl_url<- ifelse(ou_lvl==7,"LEVEL-10;","")
+  
   url<-glue::glue("{base_url}api/29/analytics.json?dimension=SH885jaRe0o:mXjFJEexCHJ;t6dWOH7W5Ml&
-                dimension=ou:LEVEL-5;LEVEL-6;LEVEL-7;LEVEL-8;LEVEL-9;LEVEL-10;{ou_uid}&
+                dimension=ou:LEVEL-5;LEVEL-6;LEVEL-7;LEVEL-8;LEVEL-9;{lvl_url}{ou_uid}&
                 dimension=dx:xNTzinnVgba;yEQ5FoXJWAx;aeCf1jJWE1x;sdarqD1J8fb;GxUQu72i38n;
                 Mon8vQgC9qg;l697bKzFRSv;J1E7eh1CyA0;LZbeWYZEkYL&
                 filter=pe:2018Oct&
@@ -104,15 +113,30 @@ analysis_getIndicatorsTable<-function(ou_uid="cDGPF739ZZr") {
   
   if (is.null(df)) { return(NULL)}
   
+  if (ou_lvl==7) {
+   
+     df %<>%
+      dplyr::select("namelevel3"=orgunitlevel3,
+                    "namelevel4"=orgunitlevel4,
+                    "namelevel5"=orgunitlevel5,
+                    "namelevel6"=orgunitlevel6,
+                    "namelevel7"=orgunitlevel7,
+                    "indicator"=dataname,
+                    "MOH"="00100 - PEPFAR-MOH align: MOH Data",
+                    "PEPFAR"="00200 - PEPFAR-MOH align: PEPFAR Data")
+  } else{
+    
+    df %<>%
+      dplyr::select("namelevel3"=orgunitlevel3,
+                    "namelevel4"=orgunitlevel4,
+                    "namelevel5"=orgunitlevel5,
+                    "namelevel6"=orgunitlevel6,
+                    "indicator"=dataname,
+                    "MOH"="00100 - PEPFAR-MOH align: MOH Data",
+                    "PEPFAR"="00200 - PEPFAR-MOH align: PEPFAR Data")
+  }
+  
   df %<>%
-    dplyr::select("namelevel3"=orgunitlevel3,
-                  "namelevel4"=orgunitlevel4,
-                  "namelevel5"=orgunitlevel5,
-                  "namelevel6"=orgunitlevel6,
-                  "namelevel7"=orgunitlevel7,
-                  "indicator"=dataname,
-                  "MOH"="00100 - PEPFAR-MOH align: MOH Data",
-                  "PEPFAR"="00200 - PEPFAR-MOH align: PEPFAR Data") %>%
     tidyr::separate(indicator,c("indicator"),sep=" ",extra='drop') %>%
     dplyr::mutate("MOH"=as.numeric(MOH)) %>%
     dplyr::mutate("PEPFAR"=as.numeric(PEPFAR)) %>%
@@ -124,11 +148,25 @@ analysis_getIndicatorsTable<-function(ou_uid="cDGPF739ZZr") {
     dplyr::mutate("PEPFAR_sum_of_sites_reporting_both" = sum(ifelse(Reported_by=="Both",PEPFAR,0))) %>%
     dplyr::mutate("Weighting" = ifelse(Reported_by=="Both",PEPFAR/PEPFAR_sum_of_sites_reporting_both,NA)) %>%
     dplyr::mutate("Average" = rowMeans(cbind(MOH, PEPFAR),na.rm=F)) %>%
-    dplyr::mutate("Weighted_diff" = ifelse(Reported_by=="Both",Weighting*abs(Difference)/Average,NA)) %>%
-    dplyr::mutate("Site_hierarchy"= paste(namelevel3,namelevel4,namelevel5,namelevel6,namelevel7,sep=" / ")) %>%
-    dplyr::select(namelevel3,namelevel4,namelevel5,namelevel6,namelevel7,indicator,
-                  MOH,PEPFAR,Reported_on_by_both,Reported_by,Reported_higher,
-                  Difference,Weighting,Weighted_diff,Count_of_sites_reporting_both,
-                  PEPFAR_sum_of_sites_reporting_both,Site_hierarchy)
+    dplyr::mutate("Weighted_diff" = ifelse(Reported_by=="Both",Weighting*abs(Difference)/Average,NA))
 
+  if (ou_lvl==7) {
+    
+    df %<>%
+      dplyr::mutate("Site_hierarchy"= paste(starts_with("namelevel"),sep=" / ")) %>%
+      dplyr::select(starts_with("namelevel"),indicator,
+                    MOH,PEPFAR,Reported_on_by_both,Reported_by,Reported_higher,
+                    Difference,Weighting,Weighted_diff,Count_of_sites_reporting_both,
+                    PEPFAR_sum_of_sites_reporting_both,Site_hierarchy)
+  } else{
+    
+    df %<>%
+      dplyr::mutate("Site_hierarchy"= paste(namelevel3,namelevel4,namelevel5,namelevel6,sep=" / ")) %>%
+      dplyr::mutate("namelevel7" = "") %>%
+      dplyr::select(starts_with("namelevel"),indicator,
+                    MOH,PEPFAR,Reported_on_by_both,Reported_by,Reported_higher,
+                    Difference,Weighting,Weighted_diff,Count_of_sites_reporting_both,
+                    PEPFAR_sum_of_sites_reporting_both,Site_hierarchy)
+  }
+  
 }
